9041d8a279ef6aad2e34704772edbb75ef9642e8
Devika Cashew <devika@cashew.ai>
2021-02-10 11:34:07 +0100

    First

---
+++ js/peanut.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("#chat-column-holder").hide();
+	$("#banner-image-holder").click(function() {
+		$("#banner-image-holder").hide();
+		$("#chat-column-holder").show();
+	});  
+});
