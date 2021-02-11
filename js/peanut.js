311a14c7e9271f360e48e0734145855b16ceffee
Devika Cashew <devika@cashew.ai>
2021-02-11 10:35:54 +0100

    Banner move to top

--- js/peanut.js
+++ js/peanut.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	$("#contentParent").hide();
+	$("#banner-image-holder-small").hide();
 	$("#banner-image-holder").click(function() {
 		$("#banner-image-holder").hide();
+		$("#banner-image-holder-small").show();
 		$("#contentParent").show();
 	});  
 });
